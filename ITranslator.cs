b753cdabeab48a53c0dda0ef9a97ba5c5ddebca3
std-nullptr <73208302+std-nullptr@users.noreply.github.com>
2021-01-05 21:22:07 -0600

    Develop (#1)
    * A bunch of changes

---
+++ ITranslator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Il2CppTranslator
+{
+    public interface ITranslator
+    {
+        public string Name { get; }
+        public List<System.Type> Dependencies { get; }
+
+        public void Initialize();
+
+        void TranslateFields() { }
+
+        void TranslateMethods() { }
+
+        void TranslateDerivedTypes() { }
+    }
+}
---
+++ TypeTranslator.cs
@@ -0,0 +1,41 @@
+﻿using Il2CppInspector.Reflection;
+using System.Collections.Generic;
+
+namespace Il2CppTranslator
+{
+    public class TypeTranslator
+    {
+        private List<FieldTranslator> _fields;
+        private TypeInfo _type;
+
+        public TypeTranslator(TypeInfo type)
+        {
+            _type = type;
+            _fields = new List<FieldTranslator>();
+        }
+
+        public void AddFieldTranslator(FieldTranslator fieldTranslator)
+        {
+            _fields.Add(fieldTranslator);
+        }
+
+        public void TranslateFields()
+        {
+            if (_type == null) return;
+            foreach (FieldTranslator translator in _fields)
+                translator.Translate(_type);
+        }
+
+        public void SetName(string type_name)
+        {
+            if (_type == null) return;
+            _type.Name = type_name;
+        }
+
+        public void SetNamespace(string type_namespace)
+        {
+            if (_type == null) return;
+            _type.Namespace = type_namespace;
+        }
+    }
+}
